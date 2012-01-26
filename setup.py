ef274ca6459ada5dddc0dbec45fdf39ea421d269
Stephan Jaekel <steph@rdev.info>
2012-01-26 20:51:55 +0100

    Bumped version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-userprofiles',
-    version='0.1',
+    version='0.2',
     description='Profiles and Registration',
     author='Stephan Jaekel',
     author_email='steph@rdev.info',
