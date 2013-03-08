1354bcc76f4683bfcbe4baca9111b740f0c48ae3
Peter Heise <info@pheise-it.de>
2013-03-09 00:40:18 +0100

    Changed emailverification views to cbv.

--- userprofiles/contrib/emailverification/views.py
+++ userprofiles/contrib/emailverification/views.py
@@ -1,45 +1,52 @@
 # -*- coding: utf-8 -*-
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 from django.utils.translation import ugettext_lazy as _
+from django.views.generic import FormView, TemplateView, RedirectView
 
 from userprofiles import settings as up_settings
 from userprofiles.contrib.emailverification.forms import ChangeEmailForm
 from userprofiles.contrib.emailverification.models import EmailVerification
+from userprofiles.mixins import LoginRequiredMixin
 
 
-@login_required
-def email_change(request):
-    if request.method == 'POST':
-        form = ChangeEmailForm(request.POST)
-        if form.is_valid():
-            verification = form.save(request.user)
-            return redirect('userprofiles_email_change_requested')
-    else:
-        form = ChangeEmailForm()
+class EmailChangeView(LoginRequiredMixin, FormView):
+    template_name = 'userprofiles/email_change.html'
+    form_class = ChangeEmailForm
 
-    return render(request, 'userprofiles/email_change.html', {'form': form})
+    def form_valid(self, form):
+        form.save(self.request.user)
+        return redirect('userprofiles_email_change_requested')
 
 
-@login_required
-def email_change_requested(request):
-    return render(request, 'userprofiles/email_change_requested.html', {
-        'expiration_days': up_settings.EMAIL_VERIFICATION_DAYS})
+email_change = EmailChangeView.as_view()
 
 
-@login_required
-def email_change_approve(request, token, code):
-    try:
-        verification = EmailVerification.objects.get(token=token, code=code,
-            user=request.user, is_expired=False, is_approved=False)
+class EmailChangeRequestedView(LoginRequiredMixin, TemplateView):
+    template_name = 'userprofiles/email_change_requested.html'
 
-        verification.is_approved = True
-        verification.save()
-        messages.success(request, _(u'E-mail address changed to %(email)s' % {
-            'email': verification.new_email}))
-    except EmailVerification.DoesNotExist:
-        messages.error(request,
-            _(u'Unable to change e-mail address. Confirmation link is invalid.'))
+    def get_context_data(self, **kwargs):
+        return {
+            'expiration_days': up_settings.EMAIL_VERIFICATION_DAYS
+        }
 
-    return redirect(up_settings.EMAIL_VERIFICATION_DONE_URL)
+
+email_change_requested = EmailChangeRequestedView.as_view()
+
+
+class EmailChangeApproveView(LoginRequiredMixin, RedirectView):
+    def get_redirect_url(self, token, code):
+        try:
+            verification = EmailVerification.objects.get(token=token, code=code,
+                user=self.request.user,is_expired=False, is_approved=False)
+            verification.is_approved = True
+            verification.save()
+            messages.success(self.request, _(u'E-mail address changed to %(email)s' % {
+                'email': verification.new_email}))
+        except EmailVerification.DoesNotExist:
+            messages.error(self.request,
+                _(u'Unable to change e-mail address. Confirmation link is invalid.'))
+        return reverse(up_settings.EMAIL_VERIFICATION_DONE_URL)
+
+email_change_approve = EmailChangeApproveView.as_view()
